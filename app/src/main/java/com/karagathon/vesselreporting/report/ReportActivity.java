d121e9925b9c6b6373ce9b47f45331414afe79cb
Tristan <rjt.mahinay@gmail.com>
2020-08-02 03:17:16 +0800

    Initial commit

---
+++ app/src/main/java/com/karagathon/vesselreporting/report/ReportActivity.java
@@ -0,0 +1,17 @@
+package com.karagathon.vesselreporting.report;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.karagathon.vesselreporting.R;
+
+public class ReportActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
