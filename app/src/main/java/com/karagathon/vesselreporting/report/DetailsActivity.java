5c1cce87a8a337b4aec42eac34b8095dd86948ab
Tristan <rjt.mahinay@gmail.com>
2020-08-12 01:29:42 +0800

    Added Google Places API, Media Data Handling and Store Key - Added Google Places - Media filenames in JSON format redirecting to Spring Boot Application upon submit - Ignore api key property file
    Signed-off-by: Tristan <rjt.mahinay@gmail.com>

--- app/src/main/java/com/karagathon/vesselreporting/report/DetailsActivity.java
+++ app/src/main/java/com/karagathon/vesselreporting/report/DetailsActivity.java
@@ -3,10 +3,13 @@ package com.karagathon.vesselreporting.report;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.widget.DatePicker;
+import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.amplifyframework.AmplifyException;
@@ -15,41 +18,64 @@ import com.amplifyframework.core.Amplify;
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.karagathon.vesselreporting.BuildConfig;
 import com.karagathon.vesselreporting.R;
 
 import org.json.JSONObject;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class DetailsActivity extends AppCompatActivity {
 
     private static final String UPLOAD_URL = "http://192.168.0.109:1331/upload";
-    private EditText dateText;
+    public static final int AUTO_PLACE_REQ_CODE = 200;
+    private EditText dateText, locationText, reportDescription;
+    private Button submitButton;
     private DatePickerDialog picker;
-    private String absoluteFilePath;
     private String currentFileName;
+    private Intent reportIntent;
+    private HashMap<String, Object> dataMap;
+    private File singleMediaFile;
+    private boolean isGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        try {
-            Amplify.addPlugin(new AWSCognitoAuthPlugin());
-            Amplify.addPlugin(new AWSS3StoragePlugin());
-            Amplify.configure(getApplicationContext());
 
-            Log.i("MyAmplifyApp", "Initialized Amplify");
-        } catch (AmplifyException e) {
-            e.printStackTrace();
+        if (Objects.isNull(savedInstanceState)) {
+            try {
+                Amplify.addPlugin(new AWSCognitoAuthPlugin());
+                Amplify.addPlugin(new AWSS3StoragePlugin());
+                Amplify.configure(getApplicationContext());
+
+                Log.i("MyAmplifyApp", "Initialized Amplify");
+            } catch (AmplifyException e) {
+                e.printStackTrace();
+            }
         }
 
+        dataMap = new HashMap<>();
+        //First Data
+        dataMap.put("Test", "Test Value");
+
         dateText = findViewById(R.id.date);
+        submitButton = findViewById(R.id.submit);
+        locationText = findViewById(R.id.location);
+        reportDescription = findViewById(R.id.reportDescription);
 
         setTitle("Title");
 
@@ -59,26 +85,83 @@ public class DetailsActivity extends AppCompatActivity {
             int month = cldr.get(Calendar.MONTH);
             int year = cldr.get(Calendar.YEAR);
 
-            picker = new DatePickerDialog(DetailsActivity.this, R.style.Theme_MaterialComponents_Light_Dialog_FixedSize, new DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-                    dateText.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
-                }
-            }, year, month, day);
+            picker = new DatePickerDialog(DetailsActivity.this, R.style.Theme_MaterialComponents_Light_Dialog_FixedSize,
+                    (datePicker, year1, month1, dayOfMonth)
+                            -> dateText.setText(dayOfMonth + "-" + (month1 + 1) + "-" + year1), year, month, day);
             picker.show();
         });
 
-        Intent intent = getIntent();
-        currentFileName = intent.getStringExtra("currentFileName");
-        absoluteFilePath = intent.getStringExtra("absoluteFilePath");
+        Places.initialize(getApplicationContext(), BuildConfig.GOOGLE_API_KEY);
 
-        Log.i("Details Current Filename", currentFileName);
-        Log.i("Details Absolute File Path", absoluteFilePath);
+        Log.i("Places is Initialized?", String.valueOf(Places.isInitialized()));
 
-        File f = new File(absoluteFilePath);
+        locationText.setOnClickListener(view -> {
+            List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME);
 
-//        Amplify.Storage.uploadFile(currentFileName, f, result -> Log.i("MyAmplifyApp", "Successfully uploaded: " + result.getKey()), storageFailure -> Log.e("MyAmplifyApp", "Upload failed", storageFailure));
+            Intent placeIntent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList).build(this);
 
+            startActivityForResult(placeIntent, AUTO_PLACE_REQ_CODE);
+        });
+
+        locationText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (locationText.length() < 1) {
+                    locationText.setError("Location is required");
+                }
+            }
+        });
+
+
+        submitButton.setOnClickListener(view -> {
+            reportIntent = getIntent();
+            isGallery = reportIntent.getBooleanExtra("isGallery", false);
+
+            if (isGallery) {
+                String[] galleryDataPaths = reportIntent.getStringArrayExtra("galleryDataPaths");
+
+                processGalleryData(galleryDataPaths);
+                uploadData(dataMap);
+
+            } else {
+//            currentFileName = reportIntent.getStringExtra("currentFileName");
+                String absoluteFilePath = reportIntent.getStringExtra("absoluteFilePath");
+
+//            Log.i("Details Current Filename", currentFileName);
+                Log.i("Details Absolute File Path", absoluteFilePath);
+
+                singleMediaFile = new File(absoluteFilePath);
+                dataMap.put("files", singleMediaFile.getName());
+
+                uploadData(dataMap);
+//            storeFilesInObjectStorage(file.getName(), file);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("Places Result Code", String.valueOf(resultCode));
+        if (requestCode == AUTO_PLACE_REQ_CODE && resultCode == RESULT_OK) {
+
+            Place place = Autocomplete.getPlaceFromIntent(data);
+
+            locationText.setText(place.getAddress());
+        } else if (requestCode == AUTO_PLACE_REQ_CODE && resultCode == RESULT_CANCELED) {
+            Log.i("User", "Cancelled");
+        }
     }
 
     @Override
@@ -90,28 +173,55 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i("On Destroy", "On Destroy");
-        File f = new File(absoluteFilePath);
-        f.delete();
+//        File f = new File(absoluteFilePath);
+//        f.delete();
+
+        if (!isGallery) {
+            singleMediaFile.delete();
+        }
+        reportIntent = null;
+        dataMap = null;
     }
 
-    private void uploadFile(Map data) {
+    private void uploadData(Map data) {
 
-        JSONObject jsonRequest = null;
-        Request request = new JsonObjectRequest(Request.Method.POST, UPLOAD_URL, new JSONObject(data), new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                //empty
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //empty
-            }
+        Request request = new JsonObjectRequest(Request.Method.POST, UPLOAD_URL, new JSONObject(data), response -> {
+            //empty
+        }, error -> {
+            //empty
         });
 
         RequestQueue rQueue = Volley.newRequestQueue(getApplicationContext());
         rQueue.add(request);
+    }
+
+    private void processGalleryData(String[] galleryDataPaths) {
+        File file;
+
+        if (Objects.isNull(galleryDataPaths)) {
+            String gallerySingleDataPath
+                    = reportIntent.getStringExtra("gallerySingleDataPath");
+            file = new File(gallerySingleDataPath);
+
+            dataMap.put("files", file.getName());
+//            storeFilesInObjectStorage(file.getName(), file);
+            return;
+        }
+
+        List<String> fileNames = new ArrayList<>(galleryDataPaths.length);
+        for (String galleryPaths : galleryDataPaths) {
+            Log.i("Gallery Details Data", galleryPaths);
+            file = new File(galleryPaths);
+            fileNames.add(file.getName());
+//            storeFilesInObjectStorage(file.getName(), file);
+        }
+
+        dataMap.put("files", fileNames);
+    }
+
+    private void storeFilesInObjectStorage(String fileName, File file) {
+        Amplify.Storage.uploadFile(fileName, file, result ->
+                        Log.i("MyAmplifyApp", "Successfully uploaded: " + result.getKey()),
+                storageFailure -> Log.e("MyAmplifyApp", "Upload failed", storageFailure));
     }
 }
